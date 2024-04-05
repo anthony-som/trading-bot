9c884cdbfb457592f7faef9660a1cc1255f013ff
anthony <qiko0425@gmail.com>
2024-04-05 01:12:22 -0400

    added stream data

--- tradingbot.py
+++ tradingbot.py
@@ -13,12 +13,13 @@ API_SECRET = os.getenv("API_SECRET")
 trading_client = TradingClient(API_KEY, API_SECRET)
 
 
-market_order_data = MarketOrderRequest(
-    symbol="BTC/USD",
-    qty=1.2,
-    side=OrderSide.BUY,
-    time_in_force=TimeInForce.GTC
-)
+from alpaca.data.live import CryptoDataStream
 
-market_order = trading_client.submit_order(market_order_data)
-print(market_order)
+stream = CryptoDataStream(API_KEY, API_SECRET)
+
+async def handle_trade(data):
+    print(data)
+    
+stream.subscribe_quotes(handle_trade, "BTC/USD")
+
+stream.run()
